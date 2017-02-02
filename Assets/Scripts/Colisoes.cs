c4afb3b0f20649119236eae4baee17e9afcd3a8a
tsgarcia <thamires.luz@outlook.com>
2017-02-02 11:25:34 -0200

    first commit

---
+++ Assets/Scripts/Colisoes.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colisoes : MonoBehaviour {
+
+	//Ao Colidir com algum objeto
+	void OnTriggerEnter2D(Collider2D obj){
+
+		switch(obj.gameObject.tag){
+
+		case "Hit":
+			print("ai");
+
+			break;
+
+		case "Objeto":
+			print ("Objeto");
+			break;
+		}
+
+		
+		
+	}
+
+	//Ao deixar de Colidir algum objeto
+	void OnCollisionExit2D(Collision2D obj){
+	
+	}
+
+	//Estao Colidindo com algum objeto
+	void OnCollisionStay2D(Collision2D obj){
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Rigidbody2D playerRigibory;
+	private float horizontal;
+	public float velocityPlayer, jumpVelocity;
+
+	// Use this for initialization
+	void Start () {
+		playerRigibory = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		horizontal = Input.GetAxisRaw ("Horizontal");
+
+		if(Input.GetButtonDown("Run")){
+			velocityPlayer = 4;
+		}
+		if(Input.GetButtonUp("Run")){
+			velocityPlayer = 2;
+		}
+		if(Input.GetButtonDown("Jump")){
+			playerRigibory.AddForce (new Vector2(0,jumpVelocity));
+		}
+	}
+
+	void FixedUpdate(){
+		playerRigibory.velocity = new Vector2 (horizontal*velocityPlayer,playerRigibory.velocity.y);
+	}
+}
